25269e3a964d8d82081a72bac9f9e73379bc8618
mstom332 <tomas@332.com.uy>
2021-08-09 21:28:23 +0000

    asd

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)    
+    if (models.storage_used == "db"):
+        cities = relationship("City", backref="state")
+    else:
+        @property
+        def cities(self):
+            new_dict = []
+            for key, value in storage.all:
+                if (self.id == value.id):
+                  new_dict[key] = value
+            return (new_dict)
