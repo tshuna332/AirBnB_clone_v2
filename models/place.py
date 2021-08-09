5e817934df6c27ea63fb4a32434e6116ea1175df
mstom332 <tomas@332.com.uy>
2021-08-09 22:48:02 +0000

    asd

--- models/place.py
+++ models/place.py
@@ -19,7 +19,6 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
     """
     if (models.storage_used == "db"):
         users = relationship("User", back_populates="Place")
