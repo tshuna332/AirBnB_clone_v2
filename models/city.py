c8e00b4efc6297465f39c584fcf1f15b9eb6d359
mstom332 <tomas@332.com.uy>
2021-08-09 22:45:52 +0000

    asd

--- models/city.py
+++ models/city.py
@@ -11,5 +11,7 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """
     if (models.storage_used == "db"):
         places = relationship("Place", backref="cities")
+    """
