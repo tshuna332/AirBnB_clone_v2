ae68a62fa02e1752f0cfe1bc145bebb625cc2cb8
mstom332 <tomas@332.com.uy>
2021-08-09 19:35:06 +0000

    fix imports

--- models/place.py
+++ models/place.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Int, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 class Place(BaseModel):
@@ -12,10 +11,10 @@ class Place(BaseModel):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Int, default=0, nullable=False)
-    number_bathrooms = Column(Int, default=0, nullable=False)
-    max_guest = Column(Int, default=0, nullable=False)
-    price_by_night = Column(Int, nullable=False, default=0)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
