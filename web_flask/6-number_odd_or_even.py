222d987987915c3a79e358a8021ca157643c7475
tshuna332 <tmmi333@gmail.com>
2021-09-12 21:41:36 +0000

    asd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,11 +44,14 @@ def number_template(n):
     return render_template("5-number.html", var=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    """FDSG FSFSDGDFGSFDG FSSFDf"""
-    return render_template('/6-number_odd_or_even.html', number=n)
-
+    """SDFDSF SFD SDF SDF SDF SD F"""
+    if (n % 2 == 0):
+        return render_template("6-number_odd_or_even.html",
+                               var="{} is even".format(n))
+    return render_template("6-number_odd_or_even.html",
+                           var="{} is odd".format(n))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
