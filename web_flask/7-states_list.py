1c928b3ff91669fc701d84e60eb4e33ae9cd3bb7
tshuna332 <tmmi333@gmail.com>
2021-09-12 22:59:43 +0000

    asd

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""SDFSD FDS FSD FSDF """
-from flask import Flask
-from flask import render_template
+"""DFSGFDSGDFSGFDG DFGDF G"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """Returns a html listing the states"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+@app.route('/states_list')
+def states_list():
+    """SDFDSF SDF SD FSDF SDF """
+    states = list(storage.all(State).values())
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def close(self):
-    """ DSFSDF SDF SDFS SDF SDFS F """
+    """SDSDFF SDFSD FSDF SDFSD"""
     storage.close()
 
 if __name__ == '__main__':
