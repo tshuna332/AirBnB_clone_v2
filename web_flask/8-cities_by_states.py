d73a1278581c372337297b3a674fad470b81cc25
tshuna332 <tmmi333@gmail.com>
2021-09-12 23:05:14 +0000

    asd

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""DFSGFDSGDFSGFDG DFGDF G"""
+"""DSFGSFDG FDGSDFGDSFD"""
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -8,17 +8,18 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """SDFDSF SDF SD FSDF SDF """
-    states = list(storage.all(State).values())
-    return render_template('7-states_list.html', states=states)
+@app.route('/cities_by_states')
+def cities_by_states():
+    """DFSGSFSD FSDF SDF SDFS"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def close(self):
-    """SDSDFF SDFSD FSDF SDFSD"""
+    """SDDFGDFGF SDFSDFSDFSDF SDFS"""
     storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
