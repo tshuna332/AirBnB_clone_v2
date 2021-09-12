abaf47046399368dfb64550754a344941bba5d51
tshuna332 <tmmi333@gmail.com>
2021-09-12 21:08:40 +0000

    asd

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""ASDA FADSDADF"""
+"""SAD DFFDSD DSFGSDFG"""
+
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,14 +8,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """DFSFGS ADS SDFGDGSD"""
+    """SFDFG DSDSF DFGFD"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """DFFDS SDF SDF S FD"""
+    """DDFSFDG FGDFGD FDG"""
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """FSDG SFDGDFSG sdfg"""
+    res = text.replace("_", " ")
+    return 'C {}'.format(res)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
