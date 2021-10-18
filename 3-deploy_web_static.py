8965a8859c6ca85dc6e42e273bf25d2b57ba1528
tshuna332 <tmmi333@gmail.com>
2021-10-18 14:43:09 +0000

    asd

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""DISKJFGDISFJDISJFDISJFDISJFDSF"""
+
+from fabric.operations import local, run, put
+from datetime import datetime
+import os
+from fabric.api import env
+import re
+
+
+env.hosts = ['35.196.90.236', '34.73.156.98']
+
+
+def do_pack():
+    """SDJFGISDFJISDOJF DISJF DISJF DISFJ DSJF"""
+    local("mkdir -p versions")
+    filename = "versions/web_static_{}.tgz".format(datetime.strftime(
+                                                   datetime.now(),
+                                                   "%Y%m%d%H%M%S"))
+    result = local("tar -cvzf {} web_static"
+                   .format(filename))
+    if result.failed:
+        return None
+    return filename
+
+
+def do_deploy(archive_path):
+    """DJSFHDSHF DSHJFIDSHJFIDSJFDISJFDS"""
+    if not os.path.exists(archive_path):
+        return False
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    res = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if res.failed:
+        return False
+    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm /tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("mv /data/web_static/releases/{}"
+              "/web_static/* /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/releases/{}/web_static"
+              .format(filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/current")
+    if res.failed:
+        return False
+    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+              .format(filename))
+    if res.failed:
+        return False
+    print('New version deployed!')
+    return True
+
+
+def deploy():
+    """AADSFD FJDS FIDSJ FIDSJFIDSJF"""
+    filepath = do_pack()
+    if filepath is None:
+        return False
+    d = do_deploy(filepath)
+    return d
