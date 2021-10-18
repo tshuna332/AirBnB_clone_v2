f4365f8c68fdf78d4191ef6a8377ee3f3481e419
tshuna332 <tmmi333@gmail.com>
2021-10-18 11:22:14 -0300

    asd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,31 +8,51 @@ env.hosts = ['35.196.90.236', '34.73.156.98']
 env.user = "ubuntu"
 
 def do_pack():
-    """asd asd as d"""
-    try:
-        local('mkdir -p versions')
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        name = "versions/web_static_" + date + ".tgz"
-        tar = local("tar -cvzf " + name + " web_static")
-        return (name)
-    except:
-        return (None)
+    """ADSFF ADSFASDFADSF"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
+
 
 def do_deploy(archive_path):
-    '''Deploy the tgz file in the server'''
-    if (not archive_path):
-        return (False)
-
-     try:
-        put(archive_path, '/tmp')
-        folder = "/data/web_static/releases/" + archive_path[9:-4] + "/"
-        run('mkdir -p ' + folder)
-        run('tar -zxvf /tmp/' + archive_path[9:] + ' -C ' + folder)
-        run('rm /tmp/' + archive_path[9:])
-        run('rm -rf /data/web_static/current')
-	run('ln -sf ' + folder + ' /data/web_static/current')
-        run('mv ' + folder_path + "web_static/* " + folder_path)
-        run('rm -rf ' + folder_path + 'web_static')
-        return (True)
-     except:
-        return (False)
+    """SDFSDF DSFDSF DSFDS FDSSD F"""
+    if not os.path.exists(archive_path):
+        return False
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    res = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
+    if res.failed:
+        return False
+    res = run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("sudo rm /tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("sudo mv /data/web_static/releases/{}"
+              "/web_static/* /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("sudo rm -rf /data/web_static/releases/{}/web_static"
+              .format(filename))
+    if res.failed:
+        return False
+    res = run("sudo rm -rf /data/web_static/current")
+    if res.failed:
+        return False
+    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+              .format(filename))
+    if res.failed:
+        return False
+    print('New version deployed!')
+    return True
